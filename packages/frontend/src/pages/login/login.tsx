b10fe2a830164d6cbdb3f71695ee04de9e4fb1b9
Ilya Kozlov <ilya@kozlov.dev>
2021-04-18 16:20:24 +0600

    feat(frontend): add register for LoginPage

--- packages/frontend/src/pages/login/login.tsx
+++ packages/frontend/src/pages/login/login.tsx
@@ -1,5 +1,5 @@
 import React, {ReactElement, useState} from "react";
-import {Redirect, Link} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import hash from "object-hash";
 import {UAParser} from "ua-parser-js";
 
@@ -16,11 +16,14 @@ const uaParser = new UAParser();
 export function Login(): ReactElement {
   const dispatch = useDispatch();
 
+  const [action, setAction] = useState<"login" | "register">("login");
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
   const accessToken = useSelector((state: Store) => state.auth.accessToken);
 
-  const login = async (e) => {
+  async function login(e) {
     e.preventDefault();
     if (!email || !password) return alert("Данные не были введены");
     const authRes = await authService.login({email, password, uaHash: hash(uaParser.getUA())});
@@ -29,40 +32,113 @@ export function Login(): ReactElement {
     dispatch(actions.auth.setAuth({accessToken, refreshToken, expiresIn}));
     const userRes = await usersService.getInfo(userId);
     dispatch(actions.user.setUser(userRes.data));
-  };
+  }
+
+  async function register(e) {
+    e.preventDefault();
+    if (!email || !password) return alert("Данные не были введены");
+    if (password !== secondPassword) return;
+
+    const authRes = await authService.register({
+      email,
+      fullName,
+      password,
+      uaHash: hash(uaParser.getUA())
+    });
+    const {accessToken, refreshToken, expiresIn, userId} = authRes.data;
+    setItemsToLocalStorage({accessToken, refreshToken, expiresIn, userId});
+    dispatch(actions.auth.setAuth({accessToken, refreshToken, expiresIn}));
+    const userRes = await usersService.getInfo(userId);
+    dispatch(actions.user.setUser(userRes.data));
+  }
+
+  function changeAction(newAction) {
+    setAction(newAction);
+    setEmail("");
+    setPassword("");
+    setSecondPassword("");
+  }
 
   if (!accessToken) {
     return (
       <div className="login-page">
         <div className="landing-wrapper">
           <div className="landing-wrapper__form-wrapper">
-            <form onSubmit={login} className="login-form">
-              <h2 className="login-form__heading">Login</h2>
-              <label className="login-form__label">Email</label>
-              <input
-                id="email"
-                className="login-form__form-control"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter your email"
-              />
-              <label className="login-form__label"> Password</label>
-              <input
-                id="password"
-                type="password"
-                className="login-form__form-control"
-                value={password}
-                placeholder="Enter your password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit" className="login-form__btn">
-                <span>Log in</span>
-              </button>
-              <Link to="/register">
-                <span className="login-form__register">Register</span>
-              </Link>
-            </form>
+            {action === "login" && (
+              <form onSubmit={login} className="login-form">
+                <h2 className="login-form__heading">Login</h2>
+                <label className="login-form__label">Email</label>
+                <input
+                  className="login-form__form-control"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                />
+                <label className="login-form__label"> Password</label>
+                <input
+                  type="password"
+                  className="login-form__form-control"
+                  value={password}
+                  placeholder="Enter your password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit" className="login-form__btn">
+                  <span>Log in</span>
+                </button>
+                <a onClick={() => changeAction("register")}>
+                  <span className="login-form__toggle-link">Register</span>
+                </a>
+              </form>
+            )}
+            {action === "register" && (
+              <form onSubmit={register} className="login-form">
+                <h2 className="login-form__heading">Register</h2>
+                <label className="login-form__label">Full name</label>
+                <input
+                  className="login-form__form-control"
+                  type="text"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                  placeholder="Enter your full name"
+                />
+                <label className="login-form__label">Email</label>
+                <input
+                  className="login-form__form-control"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                />
+                <label className="login-form__label"> Password</label>
+                <input
+                  type="password"
+                  className="login-form__form-control"
+                  value={password}
+                  placeholder="Enter your password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="login-form__label"> Repeat a password </label>
+                <input
+                  type="password"
+                  className={`login-form__form-control ${password !== secondPassword && "error"}`}
+                  value={secondPassword}
+                  placeholder="Enter your password again"
+                  onChange={(e) => setSecondPassword(e.target.value)}
+                />
+                {password !== secondPassword && (
+                  <p className="login-form__error-message" style={{color: "red"}}>
+                    Password mismatch
+                  </p>
+                )}
+                <button type="submit" className="login-form__btn">
+                  <span>Register</span>
+                </button>
+                <a onClick={() => changeAction("login")}>
+                  <span className="login-form__toggle-link">Sign in</span>
+                </a>
+              </form>
+            )}
           </div>
         </div>
       </div>
