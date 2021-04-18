b8711698725e654812b842c2b26f54b3a6f160ef
Ilya Kozlov <ilya@kozlov.dev>
2021-04-18 23:15:32 +0600

    feat(frontend): improved SearchField styles

--- packages/frontend/src/components/search-field/search-field.tsx
+++ packages/frontend/src/components/search-field/search-field.tsx
@@ -1,17 +1,16 @@
-import React, {ReactElement} from "react";
-
-import "./search-field.scss";
+import React from "react";
 import {Search as SearchIcon} from "@material-ui/icons";
+import "./search-field.scss";
 
-export class SearchField extends React.Component {
-  render(): ReactElement {
-    return (
-      <div className="search-field">
+export function SearchField(): JSX.Element {
+  return (
+    <div className="search-field">
+      <div className="search-field__control">
         <div className="search-field__icon-field">
           <SearchIcon className="search-field__icon" />
         </div>
         <input className="search-field__input" placeholder="Search or start new chat" />
       </div>
-    );
-  }
+    </div>
+  );
 }
