0d077ff87c3aaba614ace04a021c1559ba1730a8
Ilya Kozlov <ilya@kozlov.dev>
2021-05-08 16:07:57 +0600

    fix(frontend): minor changes in text panel

--- packages/frontend/src/components/text-panel/text-panel.tsx
+++ packages/frontend/src/components/text-panel/text-panel.tsx
@@ -1,11 +1,11 @@
-import React, {ReactElement, useState} from "react";
+import React, {ReactElement, useCallback, useState} from "react";
 import {Mood as MoodIcon, AttachFile as AttachFileIcon, Mic as MicIcon} from "@material-ui/icons";
 import {messagesService} from "../../api/messages/messages";
 import {IconButton} from "@material-ui/core";
 import moment from "moment";
-import "./text-panel.scss";
 import {Room} from "../../store/modules/rooms/rooms.types";
 import {User} from "../../store/modules/user/user.types";
+import "./text-panel.scss";
 
 type Props = {
   room: Room;
@@ -15,20 +15,23 @@ type Props = {
 export function TextPanel({user, room}: Props): ReactElement {
   const [message, setMessage] = useState("");
 
-  const handleSubmit = (e: React.FormEvent): void => {
-    e.preventDefault();
-    moment.locale("ru");
-    messagesService.create({
-      body: message,
-      timestamp: moment().format(),
-      images: [],
-      senderId: user._id,
-      roomId: room.id,
-      isDeleted: false,
-      isRead: false
-    });
-    setMessage("");
-  };
+  const handleSubmit = useCallback(
+    (e: React.FormEvent): void => {
+      e.preventDefault();
+      moment.locale("ru");
+      messagesService.create({
+        body: message,
+        timestamp: moment().format(),
+        images: [],
+        senderId: user._id,
+        roomId: room.id,
+        isDeleted: false,
+        isRead: false
+      });
+      setMessage("");
+    },
+    [message]
+  );
 
   return (
     <div className="text-panel">
@@ -43,7 +46,12 @@ export function TextPanel({user, room}: Props): ReactElement {
         </IconButton>
       </div>
       <div className="text-panel__input-container">
-        <form method="POST" onSubmit={handleSubmit} className="text-panel__input-box">
+        <form
+          method="POST"
+          onSubmit={handleSubmit}
+          className="text-panel__input-box"
+          autoComplete="off"
+        >
           <input
             id="message-input"
             placeholder="Enter your message"
